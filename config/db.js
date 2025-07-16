68167fe56d51176271e8c5440ccaef68d01d967a
Arun07-8 <arun08kkv@gmail.com>
2025-07-16 09:17:23 +0530

    final-changes

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
+require("dotenv").config(); // ✅ Loads .env
 const mongoose = require("mongoose");
-const env = require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI); // ✅ Connect to DB
+    console.log(process.env.MONGODB_URI); // 🔍 Show URI to confirm
     console.log("DB connected");
   } catch (error) {
     console.log("DB Connection error", error.message);
