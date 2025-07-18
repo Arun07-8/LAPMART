d15f13bdab2d3e7c4a0444bbc935f5d795ac68f3
Arun07-8 <arun08kkv@gmail.com>
2025-07-18 09:38:39 +0530

    last updates

--- config/passport.js
+++ config/passport.js
@@ -21,7 +21,7 @@ async function generateUniqueReferralCode(name) {
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://lapmart.shop/auth/google/callback",
+    callbackURL:"https://lapmart.shop/auth/google/callback",
       passReqToCallback: true,
 },
 
